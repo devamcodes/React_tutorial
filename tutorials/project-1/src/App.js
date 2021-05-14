734e426ef7859eaf404499f4c4f97ff5c9954d6b
Devam Agrawal <devamagrawal.codes@gmail.com>
2021-05-14 12:44:46 +0530

    this was created on 9/5/21 and this is main application file.

--- tutorials/project-1/src/App.js
+++ tutorials/project-1/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/greet";
+import Hello from "./components/hello";
+import Message from "./components/message";
+import Counter from "./components/counter";
+import Clicked from "./components/functionClick";
+import EventBind from "./components/eventBind";
+import ParentComponent from "./components/parentComponent";
+import UserGreeting from "./components/userGreeting";
+import NameList from "./components/nameList";
+import Inline from "./components/Inline";
+import "./appStyles.css";
+import styles from "./appStyles.module.css";
+import Form from "./components/form";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form />
+        {/* <h3 className="error">error</h3>
+        <h3 className={styles.success}>success</h3>
+        <Inline /> */}
+        {/* <NameList /> */}
+        {/* <Greet name="Sly" heroName="Slyvester" />
+        <Hello name="Sly" heroName="Slyvester" />
+        <Message />
+        <Counter />
+        <Clicked />
+        <EventBind /> */}
+        {/* <ParentComponent /> */}
+        {/* <UserGreeting /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
